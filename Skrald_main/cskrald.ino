2ac9ca831e62734f524b979acda9262b8737e8f2
carl0801 <118434635+carl0801@users.noreply.github.com>
2022-12-28 13:20:40 +0100

    Update cskrald.ino

--- Skrald_main/cskrald.ino
+++ Skrald_main/cskrald.ino
@@ -1,6 +1,6 @@
 
 //Pogram if the can is close
-char canClose(){
+void canClose(){
   turnRight(20);
   motors.setSpeeds(200,200);
   delay(320);
@@ -8,34 +8,6 @@ char canClose(){
   while(checkLineSensors() != true){
     motors.setSpeeds(200,200);
   }
-
-  /*while(true){
-    proxSensors.read();
-    prox.left = proxSensors.countsFrontWithLeftLeds();
-    prox.right = proxSensors.countsFrontWithRightLeds();
-    display.clear();
-    display.print(prox.left);
-    display.gotoXY(3, 0);
-    display.print(prox.right);
-    delay(100);
-
-    if (prox.left > prox.right){
-      //float speed_l = ((abs(diffProx)/10)*3)+2;
-      //drive.setSpeed(speed_l, 1,0);
-      motors.setSpeeds(-100,100);
-
-    }
-    else if (prox.left > prox.right){
-      //float speed_r = ((abs(diffProx)/10)*3)+2;
-      //drive.setSpeed(1, speed_r,0);
-      motors.setSpeeds(100,-100);
-    }
-    else if (prox.left == prox.right && prox.left > 2 && prox.right > 2){
-      motors.setSpeeds(200,200);
-      delay(500);
-      break;
-    }
-  }*/
 
   motors.setSpeeds(-200,-200);
   delay(500);
